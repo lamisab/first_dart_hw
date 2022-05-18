8eaa00d2b031dacac7526783dabc6451bb54a289
Lamis Abdullah <102637669+lamisab@users.noreply.github.com>
2022-05-18 13:46:31 +0300

    Add files via upload

---
+++ first_dart_hw.dart
@@ -0,0 +1,15 @@
+import 'package:first_dart_hw/first_dart_hw.dart' as first_dart_hw;
+
+void main() {
+  //github
+  const name = "Fahad Abdullah"; //name of the boy
+
+  var mobile_num = "05553333"; // his number
+
+  var DatOfBirth = 2021; //his day of birth
+
+  print(
+      ' Name : ${name} Mobile Number : ${mobile_num} Date Of Birth : ${DatOfBirth}');
+  print(mobile_num[4]);
+  print(name.length);
+}
